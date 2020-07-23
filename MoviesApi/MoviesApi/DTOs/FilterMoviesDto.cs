6e3a1ed1dcbb885e04cccaa3b94f987e56800dc3
dimitkos <User1@DESKTOP-D6C1HHM>
2020-07-23 22:56:10 +0300

    Add ordering in filtering emthod.

--- MoviesApi/MoviesApi/DTOs/FilterMoviesDto.cs
+++ MoviesApi/MoviesApi/DTOs/FilterMoviesDto.cs
@@ -12,5 +12,7 @@
         public int GenreId { get; set; }
         public bool InTheaters { get; set; }
         public bool UpcomingReleases { get; set; }
+        public string OrderingField { get; set; }
+        public bool AscendingOrder { get; set; } = true;
     }
 }
