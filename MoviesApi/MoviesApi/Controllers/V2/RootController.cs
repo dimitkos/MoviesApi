ea687164cd42e50d7c1daa3f70b99342eac0fb0d
dimitkos <User1@DESKTOP-D6C1HHM>
2020-07-28 10:30:35 +0300

    Implement versioning

--- MoviesApi/MoviesApi/Controllers/V2/RootController.cs
+++ MoviesApi/MoviesApi/Controllers/V2/RootController.cs
@@ -3,23 +3,19 @@ using MoviesApi.DTOs;
 using MoviesApi.Helpers;
 using System.Collections.Generic;
 
-namespace MoviesApi.Controllers
+namespace MoviesApi.Controllers.V2
 {
     [ApiController]
     [Route("api")]
     public class RootController : ControllerBase
     {
         [HttpGet(Name = "getRoot")]
-        [HttpHeaderIsPresent("x-version", "1")]
+        [HttpHeaderIsPresent("x-version", "2")]
         public ActionResult<IEnumerable<Link>> Get()
         {
             List<Link> links = new List<Link>();
 
             links.Add(new Link(href: Url.Link("getRoot", new { }), rel: "self", method: "GET"));
-            links.Add(new Link(href: Url.Link("createUser", new { }), rel: "create-user", method: "POST"));
-            links.Add(new Link(href: Url.Link("Login", new { }), rel: "login", method: "POST"));
-            links.Add(new Link(href: Url.Link("getGenres", new { }), rel: "get-genres", method: "GET"));
-            links.Add(new Link(href: Url.Link("getPeople", new { }), rel: "get-people", method: "GET"));
 
             return links;
         }
---
+++ MoviesApi/MoviesApi/Helpers/HttpHeaderIsPresentAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using System;
+
+namespace MoviesApi.Helpers
+{
+    public class HttpHeaderIsPresentAttribute : Attribute, IActionConstraint
+    {
+        public int Order => 0;
+        private readonly string _header;
+        private readonly string _value;
+
+        public HttpHeaderIsPresentAttribute(string header, string value)
+        {
+            _header = header;
+            _value = value;
+        }
+
+        public bool Accept(ActionConstraintContext context)
+        {
+            var headers = context.RouteContext.HttpContext.Request.Headers;
+
+            if (!headers.ContainsKey(_header))
+            {
+                return false;
+            }
+
+            return string.Equals(headers[_header], _value, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
