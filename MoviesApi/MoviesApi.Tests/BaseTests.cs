fa7c3ae72fc215a0d12fcd3c56084dac44050e00
dimitkos <User1@DESKTOP-D6C1HHM>
2020-07-31 17:42:16 +0300

    Implement unit test for Accounts controller

--- MoviesApi/MoviesApi.Tests/BaseTests.cs
+++ MoviesApi/MoviesApi.Tests/BaseTests.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MoviesApi.Helpers;
+using System.Security.Claims;
 
 namespace MoviesApi.Tests
 {
@@ -19,12 +22,28 @@ namespace MoviesApi.Tests
 
         protected IMapper BuildMap()
         {
-            var config = new MapperConfiguration(options => 
+            var config = new MapperConfiguration(options =>
             {
                 options.AddProfile(new AutoMapperProfiles());
             });
 
             return config.CreateMapper();
+        }
+
+        //this method is usefull when in an end point want to use for example user.Identity.Name
+        protected ControllerContext BuildControllerContextWithDefaultUser()
+        {
+            //mock the claims of the user
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "example@hotmail.com"),
+                new Claim(ClaimTypes.Email, "example@hotmail.com"),
+            }, "test"));
+
+            return new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
         }
     }
 }
