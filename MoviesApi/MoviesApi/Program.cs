5bee591c10cd22041e8bd4166e33e4c53bacd8bf
dimitkos <User1@DESKTOP-D6C1HHM>
2020-08-11 00:14:22 +0300

    Configure migrations in Program class

--- MoviesApi/MoviesApi/Program.cs
+++ MoviesApi/MoviesApi/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MoviesApi
 {
@@ -13,7 +9,21 @@ namespace MoviesApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var webHost = CreateHostBuilder(args).Build();
+
+            // i want between build and run to use dbcontext to run the migrations
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                //if migrations have already been run then this is not do anythng
+                //if the migration have not been run this will run all the migrations
+                //or the pending migrations that db have missing
+                context.Database.Migrate();
+            }
+
+            webHost.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
